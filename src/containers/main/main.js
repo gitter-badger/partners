70414d50bc5d7b436990f491ccdf68ecd3aacef8
yeraycaballero <yeraycaballero@gmail.com>
2016-02-05 01:09:06 +0000

    add first component and test

---
+++ src/containers/main/main.js
@@ -0,0 +1,20 @@
+import  React, { Component } from 'react';
+import Toolbar from '../../components/toolbar/toolbar';
+
+export default class Main extends Component {
+    render() {
+        return <Toolbar onSearch={this.onSearch} onEdit={this.onEdit} onNew={this.onNew} />
+    }
+
+    onSearch() {
+        console.log('search...');
+    };
+
+    onEdit() {
+        console.log('edit...');
+    };
+
+    onNew() {
+        console.log('new...');
+    };
+}
