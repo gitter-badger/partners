001a58723fd2fb458f1cbd6e025096fc3151dbb3
yeraycaballero <yeraycaballero@gmail.com>
2016-02-23 23:09:39 +0000

    add preliminary contact Profile component

---
+++ src/components/contactProfile/contactProfile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './style.css'
+
+class ContactProfile extends React.Component {
+
+    render() {
+        return (
+            <div className={style.contact}>
+                contact profile view
+            </div>
+        )
+    }
+
+}
+
+export default ContactProfile;
