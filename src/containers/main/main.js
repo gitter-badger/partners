08020b05d43278b651d2d0d05961301887c36bba
yeraycaballero <yeraycaballero@gmail.com>
2016-02-27 14:25:06 +0000

    main component reads contacts from store and handles contact selection now

--- src/containers/main/main.js
+++ src/containers/main/main.js
@@ -1,35 +1,54 @@
 import  React, { Component } from 'react';
+import { hashHistory } from 'react-router';
+
 import Toolbar from '../../components/toolbar/toolbar';
 import Contacts from '../../containers/contacts/contacts';
+import contacsStore from '../../stores/contactsStore';
 
-const contacts = [
-   {
-      'name':{
-         'first':'manuela'
-      },
-      'picture':{
-         'thumbnail':'http://api.randomuser.me/portraits/thumb/women/39.jpg'
-      }
-   },
-   {
-      'name':{
-         'first':'barbara'
-      },
-      'picture':{
-         'thumbnail':'http://api.randomuser.me/portraits/thumb/women/74.jpg'
-      }
-   }
-]
+import style from './style.css';
+
 
 export default class Main extends Component {
+    static contextTypes = {
+        router: React.PropTypes.object.isRequired
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = { contacts: [], selectedContactId: 0 };
+    }
+
+    componentDidMount() {
+        contacsStore.loadContacts().then((contacts) => {
+            if (contacts.length > 0) {
+                this.setState({ contacts: contacts , selectedContactId : contacts[0].id });
+                hashHistory.push('/contacts/' + contacts[0].id);
+            }
+        })
+    }
+
     render() {
+        const { contacts, currentContact } = this.state;
+
         return (
             <div>
-                <Toolbar onSearch={this.onSearch}
-                         onEdit={this.onEdit}
-                         onNew={this.onNew} />
-                <Contacts contacts={contacts} />
-                { this.props.children }
+                <div className={style.toolbar}>
+                    <Toolbar onSearch={this.onSearch}
+                             onEdit={this.onEdit}
+                             onNew={this.onNew}>
+                    </Toolbar>
+                </div>
+
+                <div className={style.mainPanel}>
+                    <div className="{style.navPanel}">
+                        <Contacts contacts={contacts} onSelect={this.onSelect.bind(this)}>
+                        </Contacts>
+                    </div>
+
+                    <div className={style.contentPanel}>
+                        { this.props.children }
+                    </div>
+                </div>
             </div>
         )
     }
@@ -41,6 +60,13 @@ export default class Main extends Component {
     onEdit() {
         console.log('edit...');
     };
+
+    onSelect(contactId) {
+        this.setState({
+            selectedContactId : contactId
+        });
+        hashHistory.push('/contacts/' + contactId);
+    }
 
     onNew() {
         console.log('new...');
