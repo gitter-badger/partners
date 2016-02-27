a373624f96bc2407e5c1216c92ccfd53e94da3d0
yeraycaballero <yeraycaballero@gmail.com>
2016-02-27 14:22:31 +0000

    add route contacts/:id

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Route } from 'react-router';
 
 import Main from '../containers/main/main';
-import ContactProfile from '../components/contactProfile/contactProfile';
+import ContactDetailContainer from '../containers/contactDetailContainer/contactDetailContainer';
 
 export default (
     <Route path='/' component={Main}>
-        <Route path='contacts/:id' component={ContactProfile} />
+        <Route path='contacts/:id'  component={ContactDetailContainer} />
     </Route>
 )
